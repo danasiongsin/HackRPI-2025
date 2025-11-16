25513da53f389b375478a792675c24b27f1d283e
danasiongsin <144954515+danasiongsin@users.noreply.github.com>
2025-11-15 21:30:08 -0500

    created loginscreen css file

--- frontend/src/components/LoginScreen.jsx
+++ frontend/src/components/LoginScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./LoginScreen.css";
 
 export default function LoginScreen({ onLogin }) {
   const [isRegister, setIsRegister] = useState(false);  
